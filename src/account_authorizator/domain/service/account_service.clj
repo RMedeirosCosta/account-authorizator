082f1e3a5bab5119e659f24414afccc2743d79de
Ricardo Medeiros <ricardo.junior@contaazul.com>
2019-09-11 12:19:26 -0300

    feat(account_service): implement multi-arity function at account_service

--- src/account_authorizator/domain/service/account_service.clj
+++ src/account_authorizator/domain/service/account_service.clj
@@ -2,5 +2,5 @@
     (:require [account-authorizator.domain.entity.account_entity :refer :all]))
 
 (defn create-account 
-   ;[created-accounts active-card available-limit] (if (empty? created-accounts) (create-account active-card available-limit) (create-account active-card available-limit))
-   [active-card, available-limit] (->Account active-card available-limit))
+   ([created-accounts active-card available-limit] (if (empty? created-accounts) (create-account active-card available-limit) (create-account active-card available-limit)))
+   ([active-card, available-limit] (->Account active-card available-limit)))
