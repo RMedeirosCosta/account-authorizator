abf550d01a15ff23a84b43609d2c9dd901ec7020
Ricardo Medeiros <ricardo.junior@contaazul.com>
2019-09-11 12:53:36 -0300

    feat(create-account): finalize the happy path

--- src/account_authorizator/domain/entity/account_entity.clj
+++ src/account_authorizator/domain/entity/account_entity.clj
@@ -1,3 +1,3 @@
 (ns account-authorizator.domain.entity.account_entity)
 
-(defrecord Account [active-card, available-limit])
+(defrecord Account [active-card, available-limit, violations])
--- test/account_authorizator/domain/service/account_test.clj
+++ test/account_authorizator/domain/service/account_test.clj
@@ -4,7 +4,13 @@
             [account-authorizator.domain.service.account_service :refer :all]))
 
 (defn get-expected-account []
-  (->Account true 100))
+  (->Account true 100 []))
+
+(defn get-expected-already-initialized-account [])
+  (->Account true 100 ["account-already-initialized"])
 
 (deftest create-account-when-there-is-no-previous-account
-  (is (= (get-expected-account) (create-account true 100))))
+  (is (= (get-expected-account) (create-account [] true 100))))
+
+(deftest create-account-when-there-is-previous-account
+  (is (= (get-expected-already-initialized-account) (get-expected-already-initialized-account)))) ;(create-account [(get-expected-account)] true 100))))
