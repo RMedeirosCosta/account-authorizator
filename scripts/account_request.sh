5024bca89a45d0bc42d2783596309658db5efe3f
Ricardo Medeiros <ricardo.junior@contaazul.com>
2019-09-15 22:21:34 -0300

    chore(scripts): improve acceptance test script adding the second call

--- scripts/account_request.sh
+++ scripts/account_request.sh
@@ -1,17 +1,25 @@
 create-account-request() {
   echo "curl -sX POST http://localhost:3000/accounts --data '$1' --header 'Content-type:application/json'" 
+  echo
 }
 
 get-json-with-resource-prefix() {
   echo "$(cat ../resource/$1)"
 }
 
+log-and-send-request() {
+  echo "$1"
+  eval $1
+  echo
+  echo
+}
+
 main() {
   echo "First request..."
-  local REQUEST=$(create-account-request "$(get-json-with-resource-prefix 'account_request.json')")
-  echo $REQUEST
-  eval $REQUEST
-  echo 
+  log-and-send-request "$(create-account-request "$(get-json-with-resource-prefix 'account_request.json')")"
+
+  echo "Second request..."
+  log-and-send-request "$(create-account-request "$(get-json-with-resource-prefix 'account_request_2.json')")"
 }
 
 main
