451236a7d87aaaa8bcc9afd379021c52d8b66447
Ricardo Medeiros <ricardo.junior@contaazul.com>
2019-09-15 21:42:28 -0300

    test: implement /account integration test when there is an initialized account

--- test/account_authorizator/handler_test.clj
+++ test/account_authorizator/handler_test.clj
@@ -4,17 +4,27 @@
             [account-authorizator.handler :refer [app]]
             [account-authorizator.helper.account_helper :refer [clear-database]]))
 
-(defn get-account []
+(defn get-expected-account []
   "{ \"account\": { \"activeCard\": true, \"availableLimit\": 100 }, \"violations\": [] }")
+
+(defn get-expected-already-initialized-account []
+  "{ \"account\": { \"activeCard\": true, \"availableLimit\": 100 }, \"violations\": [\"account-already-initialized\"] }")
 
 (defn account-post-request []
   (app (-> (mock/request :post "/account" "{ \"account\": { \"activeCard\": true, \"availableLimit\": 100 } }")
-                            (mock/content-type "application/json"))))
+           (mock/content-type "application/json"))))
 
 (deftest post-account-without-previous-account
     (let [response (account-post-request)
           body     (:body response)]
       (is (= (:status response) 200))
-      (is (= body (get-account)))))
+      (is (= body (get-expected-account)))))
+
+(deftest post-account-with-previous-account
+    (account-post-request)
+    (let [response (account-post-request)
+          body     (:body response)]
+      (is (= (:status response) 200))
+      (is (= body (get-expected-already-initialized-account)))))
 
 (use-fixtures :each clear-database)
