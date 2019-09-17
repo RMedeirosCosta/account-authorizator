09363ca3f8cc50ebc08a8a42272af60611a9bc1d
Ricardo Medeiros <ricardo.junior@contaazul.com>
2019-09-16 22:31:25 -0300

    refactor: extract transaction with actived card to a function

--- src/account_authorizator/domain/service/transaction_service.clj
+++ src/account_authorizator/domain/service/transaction_service.clj
@@ -6,11 +6,14 @@
 (defn remaining-limit [availableLimit, amount]
     (- availableLimit amount))
 
+(defn make-transaction-with-actived-card [account, transaction]
+    (let [remaining-limit
+        (remaining-limit (:availableLimit account) (:amount transaction))]
+        (if (> remaining-limit 0)
+            (create true remaining-limit)
+            (->Account (:activeCard account) (:availableLimit account) ["insufficient-limit"]))))
+
 (defn make-transaction [account, transaction]
     (if (not (:activeCard account))
             (->Account (:activeCard account) (:availableLimit account) ["card-not-active"])
-            (let [remaining-limit
-                (remaining-limit (:availableLimit account) (:amount transaction))]
-                (if (> remaining-limit 0)
-                    (create true remaining-limit)
-                    (->Account (:activeCard account) (:availableLimit account) ["insufficient-limit"])))))
+            (make-transaction-with-actived-card account transaction)))
