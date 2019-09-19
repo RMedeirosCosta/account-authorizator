7585ad26dbad1a2bb49bda642b5e9dd6c7b76833
Ricardo Medeiros <ricardo.junior@contaazul.com>
2019-09-18 23:55:44 -0300

    chore(script): add some calls at acceptance script

--- scripts/account_request.sh
+++ scripts/account_request.sh
@@ -1,13 +1,29 @@
+base-request() {
+  echo "curl -sX "$1" http://localhost:3000/$2 --header 'Content-type:application/json'"
+}
+
 accounts-base-request() {
-  echo "curl -sX "$1" http://localhost:3000/accounts --header 'Content-type:application/json'"
+  echo "$(base-request $1 accounts)"
+}
+
+transactions-base-request() {
+  echo "$(base-request $1 transactions)"
 }
 
 create-account-request() {
   echo "$(accounts-base-request 'POST') --data '$1'"
 }
 
+create-transaction-request() {
+  echo "$(transactions-base-request 'POST') --data '$1'"
+}
+
 get-accounts-request() {
   echo "$(accounts-base-request 'GET')"
+}
+
+get-transactions-request() {
+  echo "$(transactions-base-request 'GET')"
 }
 
 get-json-with-resource-prefix() {
@@ -25,11 +41,17 @@ main() {
   echo "First request..."
   log-and-send-request "$(create-account-request "$(get-json-with-resource-prefix 'account_request.json')")"
 
-  echo "Second request..."
-  log-and-send-request "$(create-account-request "$(get-json-with-resource-prefix 'account_request_2.json')")"
+  #echo "Second request..."
+  #log-and-send-request "$(create-account-request "$(get-json-with-resource-prefix 'account_request_2.json')")"
 
   echo "Third request..."
   log-and-send-request "$(get-accounts-request)"
+
+  #log-and-send-request "$(create-transaction-request "$(get-json-with-resource-prefix 'transaction_request.json')")"
+  #log-and-send-request "$(create-transaction-request "$(get-json-with-resource-prefix 'transaction_request_2.json')")"
+  log-and-send-request "$(create-transaction-request "$(get-json-with-resource-prefix 'transaction_request_3.json')")"
+  log-and-send-request "$(create-transaction-request "$(get-json-with-resource-prefix 'transaction_request_4.json')")"
+  log-and-send-request "$(get-transactions-request)"
 }
 
 main
